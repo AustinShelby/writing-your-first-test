b1e96d0511c24a4bc62862a87f81926bb99bddc1
Austin Shelby <helloaustinshelby@gmail.com>
2022-07-20 09:31:51 +0200

    new tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,24 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <main>
-      <h1>Hello World</h1>
-      <h2>Fruits</h2>
+      <h1 data-useless="omega" className="title" id="hello-world">
+        Hello World
+      </h1>
+      <h2 className="sub-title">Fruits</h2>
       <ul>
         <li>Apple</li>
         <li>Banana</li>
-        <li>Mango</li>
-        <li>Watermelon</li>
+        <li className="red">Mango</li>
+        <li className="blue">Watermelon</li>
       </ul>
+      <h2>Form</h2>
+      <form>
+        <label>
+          <span>Name</span>
+          <input type="text" name="name" value={"Omega"} />
+        </label>
+        <button type="submit">Submit Form</button>
+      </form>
     </main>
   );
 };
